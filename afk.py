1ac5c46b0b3e5c9393630ee5c71df3f92ac703c3
IshanGamex <85029111+IshanGamex@users.noreply.github.com>
2022-12-19 21:58:40 -0500

    Add files via upload
    When running, you can be AFK in any game and let your teammates know. This code will stop once your mouse is moved.

---
+++ afk.py
@@ -0,0 +1,62 @@
+#import mouse and keyboard
+from pynput.mouse import Button as MouseButton, Controller
+mouse = Controller()
+
+from pynput.keyboard import Key, Controller as KeyboardController
+keyboard = KeyboardController()
+
+from time import sleep
+
+sleep(5)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+for character in ' I am sorry, I will be afk for a unknown amout of time':
+    keyboard.press(character)
+    keyboard.release(character)
+    sleep(0.01)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+keyboard.press(Key.enter)
+keyboard.press(Key.shift)
+keyboard.release(Key.enter)
+keyboard.release(Key.shift)
+
+for character in ' I am afk in my spawn':
+    keyboard.press(character)
+    keyboard.release(character)
+    sleep(0.05)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+setmousepos = mouse.position
+
+while True:
+    keyboard.press('w')
+    sleep(0.5)
+    keyboard.release('w')
+    sleep(0.5)
+    if(mouse.position != setmousepos):
+        break
+    keyboard.press('a')
+    sleep(0.5)
+    keyboard.release('a')
+    sleep(0.5)
+    if(mouse.position != setmousepos):
+        break
+    keyboard.press('s')
+    sleep(0.5)
+    keyboard.release('s')
+    sleep(0.5)
+    if(mouse.position != setmousepos):
+        break
+    keyboard.press('d')
+    sleep(0.5)
+    keyboard.release('d')
+    sleep(0.5)
+    if(mouse.position != setmousepos):
+        break
